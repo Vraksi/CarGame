e2c2d91ae3e215c56d2a9e9063ce28578740bf65
Vraksi <Jakobmetalv@gmail.com>
2021-03-10 13:45:15 +0100

    Menu Ingame and Intro menu. First Pass

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+    GameObject inGameMenu;
+
+    private void Start()
+    {
+        inGameMenu = GameObject.Find("InGameMenu");
+        inGameMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            inGameMenu.SetActive(true);
+        }
+    }
+
+    public void ReturnToGame()
+    {
+        Time.timeScale = 1;
+        inGameMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
