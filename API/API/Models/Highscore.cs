167b4bf72b547fdc5593e40f24828d6a2d84e0cd
Vraksi <Jakobmetalv@gmail.com>
2021-03-17 11:40:56 +0100

    Website and API

---
+++ API/API/Models/Highscore.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace API.Models
+{
+    public class Highscore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+    }
+}
---
+++ API/API/SqlContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class SqlContext : DbContext
+    {
+        public DbSet<Highscore> Highscores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+    }
+}
